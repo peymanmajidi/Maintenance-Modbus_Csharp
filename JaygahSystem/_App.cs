4b8ee79f99dcea1b235d525f9b2cd983542993a1
Peyman Majidi <peyman.majidi@gmail.com>
2018-10-01 13:16:06 +0330

    last series

--- JaygahSystem/_App.cs
+++ JaygahSystem/_App.cs
@@ -349,6 +349,24 @@ namespace SSFGlasses
         }
 
 
+
+        public static bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            return pingable;
+        }
+
+
         public static bool DoorOpen(int door_number)
         {
             if (_App.Delta)
